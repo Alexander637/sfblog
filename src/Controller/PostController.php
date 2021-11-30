b2f7fc23981d91d7b0cf5bead0849aebcd6a0f5f
Alexander637 <sadorsadoff@gmail.com>
2021-11-30 12:55:31 +0200

    admin panel

---
+++ src/Controller/PostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\CommentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PostRepository;
+use Twig\Environment;
+
+
+class PostController extends AbstractController
+{
+
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(PostRepository $postRepository): Response
+    {
+        return new Response($this->twig->render('post/index.html.twig', [
+                        'posts' => $postRepository->findAll(),
+                    ]));
+    }
+
+
+    #[Route('/post/{id}', name: 'post')]
+    public function show(Request $request, Post $post, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($post, $offset);
+
+        return new Response($this->twig->render('post/show.html.twig', [
+            'post' => $post,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+        ]));
+    }
+}
